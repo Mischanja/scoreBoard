8b5341c6c0b873bc2970426df45c7e64e23ab4af
Michael Kulakovskij <105335157+Mischanja@users.noreply.github.com>
2023-12-07 20:39:41 +1000

    Add files via upload

---
+++ index.js
@@ -0,0 +1,45 @@
+
+
+let homeScoreEl = document.getElementById("homescore-el")
+let guestScoreEl = document.getElementById("guestscore-el")
+
+
+function guestScoreOne() {
+    
+        guestScoreEl.textContent = parseInt(guestScoreEl.textContent) + 1;
+    
+}
+
+function guestScoreTwo() {
+    
+        guestScoreEl.textContent = parseInt(guestScoreEl.textContent) + 2;
+    
+}
+
+function guestScoreThree() {
+    
+        guestScoreEl.textContent = parseInt(guestScoreEl.textContent) + 3;
+    
+}
+
+
+/// home
+
+
+function homeScoreOne() {
+    
+        homeScoreEl.textContent = parseInt(homeScoreEl.textContent) + 1;
+    
+}
+
+function homeScoreTwo() {
+    
+        homeScoreEl.textContent = parseInt(homeScoreEl.textContent) + 2;
+    
+}
+
+function homeScoreThree() {
+    
+        homeScoreEl.textContent = parseInt(homeScoreEl.textContent) + 3;
+    
+}
